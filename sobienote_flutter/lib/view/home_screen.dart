69474dbfdb936ff2b07beeb759bd5acda6c5d5fb
Eunseo-415 <eumseo@icloud.com>
2025-04-23 15:37:11 +0900

    [ADD] 홈화면 소비목표 인풋 추가, 초기 화면 세팅

--- sobienote_flutter/lib/view/home_screen.dart
+++ sobienote_flutter/lib/view/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sobienote_flutter/common/const/colors.dart';
 import 'package:sobienote_flutter/view/setting_screen.dart';
 
 import '../common/const/text_style.dart';
@@ -14,6 +15,7 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: Colors.white,
           pinned: true,
           centerTitle: true,
+          expandedHeight: kToolbarHeight + 130,
           title: GestureDetector(
             child: Row(
               mainAxisSize: MainAxisSize.min,
@@ -34,13 +36,77 @@ class HomeScreen extends StatelessWidget {
               icon: Icon(Icons.more_horiz),
             ),
           ],
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(130),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('이번 달 소비목표', style: TextStyle(fontSize: 16)),
+                        const SizedBox(height: 9),
+                        TextField(
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                          decoration: InputDecoration(
+                            hintText: '목표를 적어주세요!',
+                            hintStyle: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                            suffixIcon: Icon(
+                              Icons.arrow_circle_up_rounded,
+                              color: Colors.white,
+                            ),
+                            filled: true,
+                            fillColor: OLIVE,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide.none,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide.none,
+                            ),
+                            contentPadding:
+                            const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20), // PreferredSize 높이에 맞추기 위한 여유 공간
+                ],
+              ),
+            ),
         ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate((context, index) {
-            return ColoredBox(color: Colors.yellow,);
-          }, childCount: 16),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        ),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset('assets/images/잠자리채.png', width: 100, ),
+                SizedBox(height: 16),
+                Text('채집한 물건을 추가해 보세요'),
+              ],
+            ),
+          ),
+        )
+        // SliverGrid(
+        //   delegate: SliverChildBuilderDelegate((context, index) {
+        //     return ColoredBox(color: Colors.yellow);
+        //   }, childCount: 16),
+        //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //     crossAxisCount: 3,
+        //   ),
+        // ),
       ],
     );
   }
